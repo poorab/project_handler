7dc4a088177b300e5de3f78a901b782ca82ccfb5
poorab22 <poorabsen@gmail.com>
2015-08-03 18:03:44 +0530

    Modification in registration form, database connection established, Database population.

--- registration.php
+++ registration.php
@@ -1,5 +1,50 @@
+<!-- Including files for DB connection and Session Control -->
+<?php
+    //include 'includes/login/core.inc.php';
+    include 'includes/login/connect.inc.php';
+?>
+<!-- /End of includes -->
+
 <!DOCTYPE html>
 <html lang="en">
+
+	<?php
+	    //if(loggedIn()){ #This function is in /includes/login/core.inc.pho for verfying user session
+	      
+	    	if (isset($_POST['studentRegistration'])) {
+	    		# code...
+	    		if ((!empty($_POST['studentName'])) && (!empty($_POST['courseId'])) && (!empty($_POST['rollNo'])) && (!empty($_POST['batchId'])) && (!empty($_POST['studentEmail']))) {
+	    			# code...
+	    			$studentName = $_POST['studentName'];
+	    			$rollNo = $_POST['rollNo'];
+	    			$courseId = $_POST['courseId'];
+	    			$semester = $_POST['semester'];
+	    			$batchId = $_POST['batchId'];
+	    			$studentEmail = $_POST['studentEmail'];
+	    			$studentPassword = $_POST['studentPassword'];
+	    			$studentRegistrationQuery = mysqli_query($con, "INSERT INTO `student_details`(`student_id`, `student_name`, `roll_no`, `course_id`, `semester`, `batch_id`, `email`, `password`) VALUES ( '', '$studentName', '$rollNo', '$courseId', '$semester', '$batchId', '$email', '$password')");
+	    			echo "<script>window.alert('Student registration successful.');</script>";
+	    		}
+	    		else{
+	    			echo "<script>window.alert('Fill all the necessary fields.');</script>";
+	    		}
+	    	}
+	    	elseif (isset($_POST['mentorRegistration'])) {
+	    		# code...
+	    		if ((!empty($_POST['mentorName'])) && (!empty($_POST['mentorEmail']))) {
+	    			# code...
+	    			$mentorName = $_POST['mentorName'];
+	    			$email = $_POST['mentorEmail'];
+	    			$password = $_POST['mentorPassword'];
+	    			$studentRegistrationQuery = mysqli_query($con, "INSERT INTO `mentor_details`(`mentor_id`, `mentor_name`, `batch_id`, `email`, `password`) VALUES ( '', '$mentorName', '', '$email', '$password')");
+	    			echo "<script>window.alert('Mentor registration successful.');</script>";
+	    		}
+	    		else{
+	    			echo "<script>window.alert('Fill all the necessary fields.');</script>";
+	    		}
+	    	}
+	?>
+
 	<head>
 	
 		<meta charset="utf-8">
@@ -27,34 +72,6 @@
 		<script src="assets/js/html5shiv.js"></script>
 		<script src="assets/js/respond.min.js"></script>
 		<![endif]-->
-
-		<script type="text/javascript">
-			$(document).ready(function()
-			{
-
-			$(".tab").onClick(function()
-			{
-			var X=$(this).attr('id');
-
-			if(X=='student')
-			{
-			$("#mentor").removeClass('select');
-			$("#student").addClass('select');
-			$("#studentBox").slideUp();
-			$("#mentorBox").slideDown();
-			}
-			else
-			{
-			$("#student").removeClass('select');
-			$("#mentor").addClass('select');
-			$("#mentorBox").slideUp();
-			$("#studentBox").slideDown();
-			}
-
-			});
-
-			});
-		</script>
 
 	</head>
 
@@ -104,28 +121,150 @@
 						<h1 class="page-title">Register Now</h1>
 					</header>
 					
-					<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-						<div class="panel panel-default">
-							<div class="panel-body" id="container-tabs">
-								<div id="tabbox">
-									<a href="#" id="student" class="tab student-class">Student</a>
-									<a href="#" id="mentor" class="tab select">Mentor</a>
-								</div>
+					<div>
 
-								<div id="form-panel">
-									<!-- studentBox for student registration form -->
-									<div id="studentBox">Student Registration</div>
+						<!-- Nav tabs -->
+						<ul class="nav nav-tabs" role="tablist">
+							<li role="presentation" class="active"><a href="#student" aria-controls="student" role="tab" data-toggle="tab">Student</a></li>
+							<li role="presentation"><a href="#mentor" aria-controls="mentor" role="tab" data-toggle="tab">Mentor</a></li>
+						</ul>
 
-									<!-- mentorBox for mentor registration form-->
-									<div id="mentorBox">Mentor Registration</div>
-								</div>
+						<!-- Tab panes -->
+						<div class="tab-content">
+							<div role="tabpanel" class="tab-pane active" id="student">
+								<!-- form for student registration goes here-->
+								<form action="#" method="POST" role="form">
+									<div class="top-margin">
+										<label>Full Name <span class="text-danger">*</span></label>
+										<input type="text" placeholder="name" name="studentName" class="form-control">
+									</div>
 
-								
+									<div class="top-margin">
+										<label>Roll No <span class="text-danger">*</span></label>
+										<input type="text" placeholder="e.g. IT-2K11-01" name="rollNo" class="form-control">
+									</div>
+
+									<div class="top-margin">
+										<label>Course <span class="text-danger">*</span></label>
+										<select name="courseId">
+											<?php
+						                        $course = mysqli_query($con, "SELECT * FROM `course_details`");
+						                        while ($courseRow = mysqli_fetch_assoc($course)) {
+						                            $courseName = $courseRow['course_name'];
+						                            $courseIdValue = $courseRow['course_id'];
+						                            # iteratively generate options for brand names...
+						                            echo "<option value=".$courseIdValue.">".$courseName."</option><br>";
+						                        }
+						                    ?>
+										</select>
+									</div>
+
+									<div class="top-margin">
+										<label>Current Semester <span class="text-danger">*</span></label>
+										<select name="semester">
+											<option value="1" selected>sem 1</option>
+											<option value="2">sem 2</option>
+											<option value="3">sem 3</option>
+											<option value="4">sem 4</option>
+											<option value="5">sem 5</option>
+											<option value="6">sem 6</option>
+											<option value="7">sem 7</option>
+											<option value="8">sem 8</option>
+											<option value="9">sem 9</option>
+											<option value="10">sem 10</option>
+											<option value="11">sem 11</option>
+											<option value="12">sem 12</option>
+										</select>
+									</div>
+									
+									<div class="top-margin">
+										<label>Batch <span class="text-danger">*</span></label>
+										<select name="batchId">
+											<?php
+						                        $batch = mysqli_query($con, "SELECT * FROM `batch_details`");
+						                        while ($batchRow = mysqli_fetch_assoc($batch)) {
+						                            $batchName = $batchRow['batch_name'];
+						                            $batchIdValue = $batchRow['batch_id'];
+						                            # iteratively generate options for brand names...
+						                            echo "<option value=".$batchIdValue.">".$batchName."</option><br>";
+						                        }
+						                    ?>
+										</select>
+									</div>
+
+									<div class="top-margin">
+										<label>Email Address <span class="text-danger">*</span></label>
+										<input type="text" class="form-control" placeholder="e.g. test@example.com" name="studentEmail">
+									</div>
+
+									<div class="row top-margin">
+										<div class="col-sm-6">
+											<label>Password <span class="text-danger">*</span></label>
+											<input type="password" class="form-control">
+										</div>
+										<div class="col-sm-6">
+											<label>Confirm Password <span class="text-danger">*</span></label>
+											<input type="password" class="form-control" name="studentPassword">
+										</div>
+									</div>
+
+									<hr>
+									<div class="row">
+										<div class="col-lg-8">
+											<label class="checkbox">
+												<input type="checkbox" name="terms"> 
+												I've read the <a href="page_terms.html">Terms and Conditions</a>
+											</label>                        
+										</div>
+										<div class="col-lg-4">
+											<input class="btn btn-action" type="submit" name="studentRegistration" value="Register">
+										</div>
+									</div>
+								</form>
+
+							</div>
+							<div role="tabpanel" class="tab-pane" id="mentor">
+								<!-- form for mentor registration goes here-->
+								<form action="#" method="POST" role="form">
+									<div class="top-margin">
+										<label>Full Name <span class="text-danger">*</span></label>
+										<input type="text" placeholder="name" name="mentorName" class="form-control">
+									</div>
+
+									<div class="top-margin">
+										<label>Email Address <span class="text-danger">*</span></label>
+										<input type="text" class="form-control" placeholder="e.g. test@example.com" name="mentorEmail">
+									</div>
+
+									<div class="row top-margin">
+										<div class="col-sm-6">
+											<label>Password <span class="text-danger">*</span></label>
+											<input type="password" class="form-control">
+										</div>
+										<div class="col-sm-6">
+											<label>Confirm Password <span class="text-danger">*</span></label>
+											<input type="password" class="form-control" name="mentorPassword">
+										</div>
+									</div>
+
+									<hr>
+									<div class="row">
+										<div class="col-lg-8">
+											<label class="checkbox">
+												<input type="checkbox" name="terms"> 
+												I've read the <a href="page_terms.html">Terms and Conditions</a>
+											</label>                        
+										</div>
+										<div class="col-lg-4">
+											<input class="btn btn-action" type="submit" name="mentorRegistration" value="Register">
+										</div>
+									</div>
+								</form>
 							</div>
 						</div>
 
 					</div>
-					
+
 				</article>
 				<!-- /Article -->
 
@@ -212,5 +351,6 @@
 		<script src="assets/js/headroom.min.js"></script>
 		<script src="assets/js/jQuery.headroom.min.js"></script>
 		<script src="assets/js/template.js"></script>
+
 	</body>
 </html>
